b72bced1cff3475210b8e8d2525af174f589bd40
Abhinav Muraleedharan <abhinavmuraleedharan@gmail.com>
2024-05-15 16:19:25 -0400

    update model.py

--- src/Zero_Layer_Transformers/model.py
+++ src/Zero_Layer_Transformers/model.py
@@ -8,10 +8,9 @@ from transformers import GPT2Tokenizer
 
 batch_size = 64
 ctx_length = 8
-max_iter = 1000
 learning_rate = 1e-2
 device = 'mps'
-
+eval_iters = 200
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
@@ -32,6 +31,31 @@ val_data = data[n:]
 
 print(len(train_data))
 
+def get_batch(split):
+    # sample a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data)-ctx_length,(batch_size, ))
+    x = torch.stack([data[i:i+ctx_length] for i in ix])
+    y = torch.stack([data[i+1:i+ctx_length+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+
 class ZeroLayerModel(nn.Module):
 
     """
@@ -48,20 +72,68 @@ class ZeroLayerModel(nn.Module):
     """
     
     def __init__(self,d_model,vocab_size):
-        super().__init__()
-        self.w_e = nn.Embedding(d_model, vocab_size)
-        self.w_o = nn.Linear(vocab_size, d_model)
 
-    def forward(self,x):
+        super().__init__()
+        self.w_e = nn.Embedding(vocab_size,d_model)
+        self.w_o = nn.Linear(d_model,vocab_size)
+
+    def forward(self,x,targets=None):
         x =  self.w_e(x)
         x =  self.w_o(x)
-        return x 
+        if targets is None:
+            loss = None
+        else:
+            B,T,C = x.shape
+            x = x.view(B*T,C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(x, targets)
+        return x, loss 
+    
+    def generate(self,idx,max_tokens):
+        for _ in range(max_tokens):
+            logits, loss = self(idx)
+            logits = logits[:,-1,:]
+            print(logits)
+            probs = F.softmax(logits,dim=-1)
+            nan_mask = torch.isnan(probs)
+            # Print the tensor and the NaN mask
+            print("Tensor:", probs)
+            print("NaN Mask:", nan_mask)
+            print("Prob:",probs)
+            idx_next = torch.multinomial(probs,num_samples=1)
+            idx = torch.cat((idx,idx_next),dim=1)
+    
     
 if __name__ == "__main__":
     d_model = 100
     vocab_size = 50257
+    max_iters = 1000
+    eval_interval = 100
     model = ZeroLayerModel(d_model,vocab_size)
-    x = torch.tensor([0,2,23,10])
-    print(model(x))
+    
+    m = model.to(device)
+    context = torch.zeros((1, 1), dtype=torch.int, device=device)
+    print(tokenizer.decode(model.generate(context, max_tokens=50)[0].tolist()))
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    for iter in range(max_iters):
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        # sample a batch of data
+        xb, yb = get_batch('train')
+
+        #evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+    # Save model weights
+    torch.save(model.state_dict(), 'model_weights.pth')
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(tokenizer.decode(model.generate(context, max_tokens=500)[0].tolist()))
 
     
