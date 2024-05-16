8b4e4a805ca203fad0a5cf24231e9fcb95dd8ff0
Abhinav Muraleedharan <abhinavmuraleedharan@gmail.com>
2024-05-16 03:01:31 +0000

    added inference code

--- src/One_Layer_Transformers/inference.py
+++ src/One_Layer_Transformers/inference.py
@@ -17,7 +17,7 @@ model = OneLayerModel(d_model, vocab_size).to(device)
 model.load_state_dict(torch.load('model_weights_2.pth'))
 model.eval()
 
-text_input = "Would you kill a human ?"
+text_input = "Is 1+1=2?"
 context = torch.tensor([tokenizer.encode(text_input)], device=device)
 
 print(tokenizer.decode(model.generate(context, max_tokens=500)[0].tolist()))
