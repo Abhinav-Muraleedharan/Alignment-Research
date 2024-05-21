2cb62a529094798fdf95ab2171ec0a62c7945cf4
Abhinav Muraleedharan <abhinavmuraleedharan@gmail.com>
2024-05-21 14:59:33 -0400

    added jax linearization code

---
+++ Scratchpad/jax_linearize.py
@@ -0,0 +1,59 @@
+"""
+
+Sample Code to study linearization
+in jax on toy networks
+
+Note: Linearization is trivial for small networks.
+We just have to call jax.linearize() and extract 
+the jacobian vector product and output value of 
+network. 
+
+"""
+import jax
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+from jax import random
+from jax.scipy.special import logsumexp
+
+
+# function to randomly initialize weights and biases.
+def random_layer_params(m,n,key,scale=1e-2):
+    w_key, b_key = random.split(key)
+    return scale*random.normal(w_key,(n,m)) , scale*random.normal(b_key,(n,))
+
+def init_network_params(sizes, key):
+  keys = random.split(key, len(sizes))
+  return [random_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+
+def relu(x):
+  return jnp.maximum(0, x)
+
+
+def network(params, x):
+  # per-example predictions
+  activations = x
+  for w, b in params[:-1]:
+    outputs = jnp.dot(w, activations) + b
+    activations = relu(outputs)
+  
+  final_w, final_b = params[-1]
+  logits = jnp.dot(final_w, activations) + final_b
+  return logits - logsumexp(logits)
+
+
+if __name__ == '__main__':
+    
+    layer_sizes = [784, 512, 512, 10]
+    step_size = 0.01
+    num_epochs = 10
+    batch_size = 128
+    n_targets = 10
+    params = init_network_params(layer_sizes, random.key(0))
+    x = random.normal(random.key(1), (28 * 28,))
+    output = network(params,x)
+    print(output)
+    y, f_jvp = jax.linearize(network,params,x)
+    print(y)
+    x_bar = jnp.zeros((28*28,))
+    print(f_jvp(params,x_bar))
--- readme.txt
+++ readme.txt
@@ -3,20 +3,16 @@ Alignment Research:
 
 General Directions:
 
-1. Implement:-
-    1.1 Zero Layer Transformers
-    2.1 One  Layer Transformers [Attention Only]
-    2.3 Two  Layer Transformers [Attention Only]
+More generally, we are trying to 'understand' an unsafe/unligned model by taking a taylor approximation
+around safe model. Now 'understand' is a sloppy word, so let's define the research questions more precisely:
 
-What is the goal of experiment ? 
-
-First of all, I would like to understand the architecture in detail. I have a good sense of things - but my understanding isn't complete.
-
-Second, I need to understand how the directions in the activation space relate to different emergent qualities of the model. 
-
-Third, I would like to test unitary rotation layer hypothesis. 
+1. Can we compute directions in the activation space that correspond to unsafe behaviours ?
+2. Can we compute a subspace in the activation space that would correspond to safe/good behaviours? 
+3. Many techniques exist for computing meaningful directions in the activation space. What is the 
+advantage of our method as opposed to existing appraoches like applying a linear probe etc ? Taking a taylor 
+approximation and arrguing from that is theoretically more rigorous, but can we apply the insights from theory
+to improve the model in  a significant way ? 
 
 
-Plenty of details 
--------------------- Finetuning dynamics
--------------------- NTK approximation 
+
+
